502561ef301e33303ed2250247bce7b25f43914c
AnaBeatrizRomera <ana.romera@germinare.org.br>
2024-09-07 17:13:45 -0300

    Subindo

--- src/main/java/com/example/leontisredis/service/UsuarioService.java
+++ src/main/java/com/example/leontisredis/service/UsuarioService.java
@@ -9,6 +9,8 @@ public class UsuarioService {
 
     private final RedisTemplate<String, Object> redisTemplate;
 
+    private static final Logger logger = LoggerFactory.getLogger(UsuarioService.class);
+
     public UsuarioService(RedisTemplate<String, Object> redisTemplate) {
         this.redisTemplate = redisTemplate;
     }
@@ -23,11 +25,18 @@ public class UsuarioService {
         redisTemplate.opsForValue().decrement("atividade");
     }
 
-    // Obter a contagem de atividade do usuário
     public Integer getUserActivity() {
+        try {
             String countStr = (String) redisTemplate.opsForValue().get("atividade");
             Integer count = countStr != null ? Integer.valueOf(countStr) : 0;
+            logger.info("Contagem de atividade obtida com sucesso: {}", count);
             return count;
-
+        } catch (NumberFormatException e) {
+            logger.error("Erro ao converter a contagem de atividade", e);
+            throw new RuntimeException("Erro ao converter a contagem de atividade", e);
+        } catch (Exception e) {
+            logger.error("Erro ao buscar contagem de atividade do usuário", e);
+            throw e;
+        }
     }
 }
